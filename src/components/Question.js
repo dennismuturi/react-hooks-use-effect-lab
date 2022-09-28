6238091d0114464f54931994f752ca16d2b56209
dennismuturi <dennismuturi1@gmail.com>
2022-09-28 10:02:59 +0300

    first commit

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState ,use} from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(()=>{
+    if(!timeRemaining < 0){
+    setTimeout(setTimeRemaining(timeRemaining-100),100);
+    }else{
+      setTimeRemaining(1000);
+      onAnswered(false);
+    }
+  },[timeRemaning])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
